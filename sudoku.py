8ba77d1d7431cf6fa59834b8297e683f48fffe7b
James Chabot-Weingart <jcw@uconn.edu>
2022-01-11 00:25:25 -0500

    Argparse, extractpuzzle added. Need to add mutual exclusion to -p and -i, as well as file processing

--- sudoku.py
+++ sudoku.py
@@ -1,62 +1,71 @@
+import argparse
 from constraint import *
 
-problem = Problem()
-domain = range(1,10)
-cols = range(9)
-rows = range(9)
+def solve(puzzle):
 
-grid = []
+  problem = Problem()
+  domain = range(1,10)
+  cols = range(9)
+  rows = range(9)
 
-for row in rows: #Create all 81 variables and add uniqueness constraint for each row
-    grid.append([])
-    for column in cols:    
-        grid[row].append(str(row) +','+ str(column))
-    problem.addVariables(grid[row], domain)
-    problem.addConstraint(AllDifferentConstraint(), grid[row])
+  grid = []
 
-collist = []
-for column in cols: #Add uniqueness constraint for each column
-    for row in rows:
-        collist.append(grid[row][column])
-    problem.addConstraint(AllDifferentConstraint(), collist)
-    collist = []
+  for row in rows: #Create all 81 variables and add uniqueness constraint for each row
+      grid.append([])
+      for column in cols:    
+          grid[row].append(str(row) +','+ str(column))
+      problem.addVariables(grid[row], domain)
+      problem.addConstraint(AllDifferentConstraint(), grid[row])
 
-for row in [0,3,6]: #Add uniqueness constraint for each house
-    for column in [0,3,6]:
-        problem.addConstraint(AllDifferentConstraint(), [grid[row][column],   grid[row+1][column],   grid[row+2][column],\
-                                                         grid[row][column+1], grid[row+1][column+1], grid[row+2][column+1],\
-                                                         grid[row][column+2], grid[row+1][column+2], grid[row+2][column+2]])
+  collist = []
+  for column in cols: #Add uniqueness constraint for each column
+      for row in rows:
+          collist.append(grid[row][column])
+      problem.addConstraint(AllDifferentConstraint(), collist)
+      collist = []
 
-problem.addConstraint(ExactSumConstraint(1), ['0,8'])
-problem.addConstraint(ExactSumConstraint(2), ['1,5'])
-problem.addConstraint(ExactSumConstraint(3), ['1,7'])
-problem.addConstraint(ExactSumConstraint(4), ['1,8'])
-problem.addConstraint(ExactSumConstraint(5), ['2,1'])
-problem.addConstraint(ExactSumConstraint(6), ['2,2'])
-problem.addConstraint(ExactSumConstraint(4), ['3,4'])
-problem.addConstraint(ExactSumConstraint(5), ['3,6'])
-problem.addConstraint(ExactSumConstraint(4), ['4,2'])
-problem.addConstraint(ExactSumConstraint(1), ['4,4'])
-problem.addConstraint(ExactSumConstraint(7), ['5,1'])
-problem.addConstraint(ExactSumConstraint(8), ['5,6'])
-problem.addConstraint(ExactSumConstraint(6), ['6,3'])
-problem.addConstraint(ExactSumConstraint(2), ['6,8'])
-problem.addConstraint(ExactSumConstraint(8), ['7,3'])
-problem.addConstraint(ExactSumConstraint(9), ['7,6'])
-problem.addConstraint(ExactSumConstraint(4), ['8,0'])
+  for row in [0,3,6]: #Add uniqueness constraint for each house
+      for column in [0,3,6]:
+          problem.addConstraint(AllDifferentConstraint(), [grid[row][column],   grid[row+1][column],   grid[row+2][column],\
+                                                           grid[row][column+1], grid[row+1][column+1], grid[row+2][column+1],\
+                                                           grid[row][column+2], grid[row+1][column+2], grid[row+2][column+2]])
 
-solution = problem.getSolution()
-
-#pretty printing of solved puzzle
-result = "+ - - - + - - - + - - - +\n"
-for row in rows:
-  result += '| '
-  for column in cols:
-      result += str(solution[grid[row][column]]) + ' '
-      if column %3 == 2:
-          result += '| '
-  result+= '\n'
-  if row %3 == 2:
-      result += "+ - - - + - - - + - - - +\n"
+  for (x,y) in puzzle:
+    problem.addConstraint(ExactSumConstraint(x), y)
   
-print (result)
+  solution = problem.getSolution()
+
+  #pretty printing of solved puzzle
+  result = '+ - - - + - - - + - - - +\n'
+  for row in rows:
+    result += '| '
+    for column in cols:
+        result += str(solution[grid[row][column]]) + ' '
+        if column %3 == 2:
+            result += '| '
+    result+= '\n'
+    if row %3 == 2:
+        result += '+ - - - + - - - + - - - +\n'
+    
+  return (result)
+  
+def extractpuzzle(puzzlestring):
+  x = 0
+  puzzle = []
+  for i in puzzlestring:
+    if i > 0:
+      puzzle.append(int(i), (int(x/9), x%9))
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Find Sudoku puzzle solution')
+  #add mutually exclusive group (https://docs.python.org/3/library/argparse.html#mutual-exclusion)
+  parser.add_argument('-p', '--puzzle', dest='puzzlestring', help='81 digit puzzle string')
+  parser.add_argument('-i', '--input-file', dest='filename', help='a text file containing 1 or more 81 digit puzzle strings, separated by line breaks')
+  args = parser.parse_args()
+  
+  if args.puzzlestring not null:
+    print(solve(extractpuzzle(args.puzzlestring)))
+  
+if __name__ == '__main__':
+   main()
