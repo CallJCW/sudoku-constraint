1d5442234d9ac4c8bc0802b7949456380a5851c2
James Chabot-Weingart <calljcw@gmail.com>
2022-01-12 23:58:13 -0500

    moved extractpuzzle(puzzlestring) call inside solve(puzzlestring), so that we can pass around puzzlestrings. file handling and basic error checking in place. Need to add error handling to extractpuzzle(puzzlestring) for non-number inputs

--- sudoku.py
+++ sudoku.py
@@ -1,13 +1,38 @@
 import argparse
+import os
 from constraint import *
 
-def solve(puzzle):
+def prettysudoku(puzzlestring):
+  cols = range(9)
+  rows = range(9)
 
+  result = '+ - - - + - - - + - - - +\n'
+  for row in rows:
+    result += '| '
+    for column in cols:
+        result += str(puzzlestring[row*9+column]) + ' '
+        if column %3 == 2:
+            result += '| '
+    result+= '\n'
+    if row %3 == 2:
+        result += '+ - - - + - - - + - - - +\n'
+        
+  return (result)
+  
+def extractpuzzle(puzzlestring):
+  x = 0
+  puzzle = []
+  for i in puzzlestring:
+    if int(i) > 0:
+      puzzle.append((int(i), (str(int(x/9)) + ',' + str(x%9) ) ) )
+    x += 1
+  return (puzzle)
+
+def solve(puzzlestring):
   problem = Problem()
   domain = range(1,10)
   cols = range(9)
   rows = range(9)
-
   grid = []
 
   for row in rows: #Create all 81 variables and add uniqueness constraint for each row
@@ -30,32 +55,18 @@ def solve(puzzle):
                                                            grid[row][column+1], grid[row+1][column+1], grid[row+2][column+1],\
                                                            grid[row][column+2], grid[row+1][column+2], grid[row+2][column+2]])
 
+  puzzle = extractpuzzle(puzzlestring)
   for (x,y) in puzzle:
-    problem.addConstraint(ExactSumConstraint(x), y)
+    problem.addConstraint(ExactSumConstraint(x), [y])
   
   solution = problem.getSolution()
 
-  #pretty printing of solved puzzle
-  result = '+ - - - + - - - + - - - +\n'
+  solvedpuzzlestring = ''
   for row in rows:
-    result += '| '
     for column in cols:
-        result += str(solution[grid[row][column]]) + ' '
-        if column %3 == 2:
-            result += '| '
-    result+= '\n'
-    if row %3 == 2:
-        result += '+ - - - + - - - + - - - +\n'
-    
-  return (result)
-  
-def extractpuzzle(puzzlestring):
-  x = 0
-  puzzle = []
-  for i in puzzlestring:
-    if i > 0:
-      puzzle.append(int(i), (int(x/9), x%9))
+        solvedpuzzlestring += str(solution[grid[row][column]])
 
+  return (solvedpuzzlestring)
 
 def main():
   parser = argparse.ArgumentParser(description='Find Sudoku puzzle solution')
@@ -64,8 +75,21 @@ def main():
   parser.add_argument('-i', '--input-file', dest='filename', help='a text file containing 1 or more 81 digit puzzle strings, separated by line breaks')
   args = parser.parse_args()
   
-  if args.puzzlestring not null:
-    print(solve(extractpuzzle(args.puzzlestring)))
+  if args.puzzlestring:
+    if len(args.puzzlestring) != 81:
+      print('puzzlestring not valid length', args.puzzlestring)
+    else:
+      print(prettysudoku(solve(args.puzzlestring)))
+  if args.filename:
+    if not os.path.isfile(args.filename):
+      print('File does not exist')
+    else:
+      for line in open(args.filename, 'r').readlines():
+        line = line.strip()
+        if len(line) != 81:
+          print('puzzlestring not valid length', str(len(line) ), line)
+        else:
+          print(prettysudoku(solve(line)))
   
 if __name__ == '__main__':
    main()
